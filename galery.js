aa425e0c48b164627e249c7eccac635bbb1f6a5d
Esteban <estebanoller73@gmail.com>
2024-01-07 01:31:05 -0300

    implementacion de peacock

--- galery.js
+++ galery.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
         { url: 'public/img/diario-mk/peacock/3.jpg', category: 'articulos' },
         { url: 'public/img/diario-mk/peacock/3.5.jpg', category: 'articulos' },
         { url: 'public/img/diario-mk/peacock/4.jpg', category: 'articulos' },
-        { url: 'public/img/diario-mk/peacock/Screenshot 2024-01-06 211005.jpg', category: 'articulos' },
+        { url: 'public/img/diario-mk/peacock/5.jpg', category: 'articulos' },
         /** FIN  **/
 
         /** DIARIO-MK **/
